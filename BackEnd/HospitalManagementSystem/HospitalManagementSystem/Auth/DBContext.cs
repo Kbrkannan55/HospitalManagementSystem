21495cb163f47b85cbf1dfc30594f82ff5eb4055
Boopathiraja Kannan <127291904+Kbrkannan55@users.noreply.github.com>
2023-06-30 11:26:45 +0530

    AuthDB

---
+++ BackEnd/HospitalManagementSystem/HospitalManagementSystem/Auth/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bware.Auth
+{
+    public class DBContext : IdentityDbContext<ApplicationUser>
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TokenInfo> TokenInfo { get; set; }
+    }
+}
---
+++ BackEnd/HospitalManagementSystem/HospitalManagementSystem/Auth/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bware.Auth
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Key]
+        public string? Username { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
